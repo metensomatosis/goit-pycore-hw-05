6884d4e9c380057f10754e1ffd726d4472991c0b
metensomatosis <vkrasnovskyy@gmail.com>
2026-03-15 22:52:21 +0200

    Update Module8_T2.py

--- Module8_T2.py
+++ Module8_T2.py
@@ -3,17 +3,16 @@ from typing import Callable, Generator
 
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    pattern = r"\b\d+\.\d+\b"
+    """
+    Повертає генератор дійсних чисел, знайдених у тексті.
+    Числа мають бути відокремлені пробілами або знаками пунктуації.
+    """
+    pattern: str = r"\b\d+\.\d+\b"
 
     for match in re.finditer(pattern, text):
         yield float(match.group())
 
 
-def sum_profit(text: str, func: Callable) -> float:
-    return sum(func(text))
-
-
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-
-total_income = sum_profit(text, generator_numbers)
-print(f"Загальний дохід: {total_income}")
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    """
+    Обчислює загальну суму всіх чисел, які повертає функція-генератор.
