5a74cab4a62a35ee1bf8a7df571d6afc13f7a575
metensomatosis <vkrasnovskyy@gmail.com>
2026-03-18 21:10:13 +0200

    Update Module8_T2.py

--- Module8_T2.py
+++ Module8_T2.py
@@ -5,7 +5,6 @@ from typing import Callable, Generator
 def generator_numbers(text: str) -> Generator[float, None, None]:
     """
     Повертає генератор дійсних чисел, знайдених у тексті.
-    Числа мають бути відокремлені пробілами або знаками пунктуації.
     """
     pattern: str = r"\b\d+\.\d+\b"
 
@@ -16,3 +15,16 @@ def generator_numbers(text: str) -> Generator[float, None, None]:
 def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
     """
     Обчислює загальну суму всіх чисел, які повертає функція-генератор.
+    """
+    return sum(func(text))
+
+
+if __name__ == "__main__":
+    text: str = (
+        "Загальний дохід працівника складається з декількох частин: "
+        "1000.01 як основний дохід, доповнений додатковими "
+        "надходженнями 27.45 і 324.00 доларів."
+    )
+
+    total_income: float = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")
